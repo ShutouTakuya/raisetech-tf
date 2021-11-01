1201b01f15448918d193dad55c75e86d4d2adbac
takuya <8vc3260105@gmail.com>
2021-11-01 21:11:29 +0900

    sorry page用のS3 Bucketを作成

--- ec2.tf
+++ ec2.tf
@@ -1,164 +1,137 @@
-# -------------------------------------
-# Keypair
-# -------------------------------------
-# resource "aws_key_pair" "keypair" {
-#   key_name   = "${var.project}-${var.env}-keypair"
-#   public_key = file("./keys/raisetech_tf_key.pub")
+# # -------------------------------------
+# # Keypair
+# # -------------------------------------
+# # resource "aws_key_pair" "keypair" {
+# #   key_name   = "${var.project}-${var.env}-keypair"
+# #   public_key = file("./keys/raisetech_tf_key.pub")
 
-#   tags = {
-#     Name    = "${var.project}-${var.env}-keypair"
-#     Project = var.project
-#     Env     = var.env
-#   }
-# }
+# #   tags = {
+# #     Name    = "${var.project}-${var.env}-keypair"
+# #     Project = var.project
+# #     Env     = var.env
+# #   }
+# # }
+
+# # # -------------------------------------
+# # # SSM Parameter Store
+# # # -------------------------------------
+# # resource "aws_ssm_parameter" "db_host" {
+# #   name  = "/${var.project}/${var.env}/app/MYSQL_HOST"
+# #   type  = "string"
+# #   value = aws_db_instance.mysql_standalone.address
+# # }
+# # resource "aws_ssm_parameter" "db_port" {
+# #   name  = "/${var.project}/${var.env}/app/MYSQL_PORT"
+# #   type  = "string"
+# #   value = aws_db_instance.mysql_standalone.port
+# # }
+# # resource "aws_ssm_parameter" "db_name" {
+# #   name  = "/${var.project}/${var.env}/app/MYSQL_DATABASE"
+# #   type  = "string"
+# #   value = aws_db_instance.mysql_standalone.name
+# # }
+# # resource "aws_ssm_parameter" "db_username" {
+# #   name  = "/${var.project}/${var.env}/app/MYSQL_USERNAME"
+# #   type  = "SecureString"
+# #   value = aws_db_instance.mysql_standalone.username
+# # }
+# # resource "aws_ssm_parameter" "db_password" {
+# #   name  = "/${var.project}/${var.env}/app/MYSQL_PASSWORD"
+# #   type  = "SecureString"
+# #   value = aws_db_instance.mysql_standalone.password
+# # }
+
+# # # -------------------------------------
+# # # EC2 Instance
+# # # -------------------------------------
+# # resource "aws_instance" "ap_server" {
+# #   ami                  = data.aws_ami.app.id
+# #   instance_type        = "t2.micro"
+# #   subnet_id            = aws_subnet.web_app_private_subnet_1a.id
+# #   iam_instance_profile = aws_iam_instance_profile.app_ec2_profile.name
+# #   vpc_security_group_ids = [
+# #     aws_security_group.app_sg.id,
+# #     aws_security_group.opmng_sg.id
+# #   ]
+# #   key_name = aws_key_pair.keypair.key_name
+
+# #   tags = {
+# #     Name    = "${var.project}-${var.env}-ap-server-1a"
+# #     Project = var.project
+# #     Env     = var.env
+# #     Type    = "app"
+# #   }
+# # }
 
 # # -------------------------------------
-# # SSM Parameter Store
+# # Launch Template
 # # -------------------------------------
-# resource "aws_ssm_parameter" "db_host" {
-#   name  = "/${var.project}/${var.env}/app/MYSQL_HOST"
-#   type  = "string"
-#   value = aws_db_instance.mysql_standalone.address
-# }
-# resource "aws_ssm_parameter" "db_port" {
-#   name  = "/${var.project}/${var.env}/app/MYSQL_PORT"
-#   type  = "string"
-#   value = aws_db_instance.mysql_standalone.port
-# }
-# resource "aws_ssm_parameter" "db_name" {
-#   name  = "/${var.project}/${var.env}/app/MYSQL_DATABASE"
-#   type  = "string"
-#   value = aws_db_instance.mysql_standalone.name
-# }
-# resource "aws_ssm_parameter" "db_username" {
-#   name  = "/${var.project}/${var.env}/app/MYSQL_USERNAME"
-#   type  = "SecureString"
-#   value = aws_db_instance.mysql_standalone.username
-# }
-# resource "aws_ssm_parameter" "db_password" {
-#   name  = "/${var.project}/${var.env}/app/MYSQL_PASSWORD"
-#   type  = "SecureString"
-#   value = aws_db_instance.mysql_standalone.password
-# }
-
-# # -------------------------------------
-# # EC2 Instance
-# # -------------------------------------
-# resource "aws_instance" "ap_server" {
-#   ami                  = data.aws_ami.app.id
-#   instance_type        = "t2.micro"
-#   subnet_id            = aws_subnet.web_app_private_subnet_1a.id
-#   iam_instance_profile = aws_iam_instance_profile.app_ec2_profile.name
-#   vpc_security_group_ids = [
-#     aws_security_group.app_sg.id,
-#     aws_security_group.opmng_sg.id
-#   ]
-#   key_name = aws_key_pair.keypair.key_name
-
-#   tags = {
-#     Name    = "${var.project}-${var.env}-ap-server-1a"
-#     Project = var.project
-#     Env     = var.env
-#     Type    = "app"
-#   }
-# }
-
-# -------------------------------------
-# Launch Template
-# -------------------------------------
-# Webサーバー用の起動テンプレート
-resource "aws_launch_template" "web_server_lt" {
-  update_default_version = true
-  name                   = "${var.project}-${var.env}-web-server-lt"
-  image_id               = var.my_ami_id
-  key_name               = var.key_name
-  #image_id               = data.aws_ami.web_server.image_id
-
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name    = "${var.project}-${var.env}-web-server-from-asg"
-      Project = var.project
-      Env     = var.env
-      Type    = "web"
-    }
-  }
-
-  network_interfaces {
-    associate_public_ip_address = false
-    security_groups = [
-      aws_security_group.web_sg.id,
-      aws_security_group.opmng_sg.id
-    ]
-    delete_on_termination = true
-  }
-}
-
-# APサーバー用の起動テンプレート
-# resource "aws_launch_template" "ap_server_lt" {
+# # Webサーバー用の起動テンプレート
+# resource "aws_launch_template" "web_server_lt" {
 #   update_default_version = true
-#   name                   = "${var.project}-${var.env}-ap-server-lt"
+#   name                   = "${var.project}-${var.env}-web-server-lt"
 #   image_id               = var.my_ami_id
 #   key_name               = var.key_name
-#   #image_id               = data.aws_ami.ap_server.image_id
+#   #image_id               = data.aws_ami.web_server.image_id
 
 #   tag_specifications {
 #     resource_type = "instance"
 #     tags = {
-#       Name    = "${var.project}-${var.env}-ap-server-from-asg"
+#       Name    = "${var.project}-${var.env}-web-server-from-asg"
 #       Project = var.project
 #       Env     = var.env
-#       Type    = "app"
+#       Type    = "web"
 #     }
 #   }
 
 #   network_interfaces {
 #     associate_public_ip_address = false
 #     security_groups = [
-#       aws_security_group.app_sg.id,
+#       aws_security_group.web_sg.id,
 #       aws_security_group.opmng_sg.id
 #     ]
 #     delete_on_termination = true
 #   }
-
-#   iam_instance_profile {
-#     name = aws_iam_instance_profile.app_ec2_profile.name
-#   }
 # }
 
-# -------------------------------------
-# Auto Scaring Group
-# -------------------------------------
-# Webサーバー用のAuto Scaring Group
-resource "aws_autoscaling_group" "web_server_asg" {
-  name                      = "${var.project}-${var.env}-asg-for-web-server"
-  max_size                  = 4
-  min_size                  = 2
-  desired_capacity          = 2
-  health_check_grace_period = 300
-  health_check_type         = "ELB"
-  vpc_zone_identifier = [
-    aws_subnet.web_app_private_subnets[0].id,
-    aws_subnet.web_app_private_subnets[1].id
-  ]
-  target_group_arns = [aws_lb_target_group.external_alb_target_group.arn]
+# # APサーバー用の起動テンプレート
+# # resource "aws_launch_template" "ap_server_lt" {
+# #   update_default_version = true
+# #   name                   = "${var.project}-${var.env}-ap-server-lt"
+# #   image_id               = var.my_ami_id
+# #   key_name               = var.key_name
+# #   #image_id               = data.aws_ami.ap_server.image_id
 
-  mixed_instances_policy {
-    launch_template {
-      launch_template_specification {
-        launch_template_id = aws_launch_template.web_server_lt.id
-        version            = "$Latest"
-      }
-      override {
-        instance_type = var.default_instance_type
-      }
-    }
-  }
-}
+# #   tag_specifications {
+# #     resource_type = "instance"
+# #     tags = {
+# #       Name    = "${var.project}-${var.env}-ap-server-from-asg"
+# #       Project = var.project
+# #       Env     = var.env
+# #       Type    = "app"
+# #     }
+# #   }
 
-# APサーバー用のAuto Scaring Group
-# resource "aws_autoscaling_group" "ap_server_asg" {
-#   name                      = "${var.project}-${var.env}-asg-for-ap-server"
+# #   network_interfaces {
+# #     associate_public_ip_address = false
+# #     security_groups = [
+# #       aws_security_group.app_sg.id,
+# #       aws_security_group.opmng_sg.id
+# #     ]
+# #     delete_on_termination = true
+# #   }
+
+# #   iam_instance_profile {
+# #     name = aws_iam_instance_profile.app_ec2_profile.name
+# #   }
+# # }
+
+# # -------------------------------------
+# # Auto Scaring Group
+# # -------------------------------------
+# # Webサーバー用のAuto Scaring Group
+# resource "aws_autoscaling_group" "web_server_asg" {
+#   name                      = "${var.project}-${var.env}-asg-for-web-server"
 #   max_size                  = 4
 #   min_size                  = 2
 #   desired_capacity          = 2
@@ -168,12 +141,12 @@ resource "aws_autoscaling_group" "web_server_asg" {
 #     aws_subnet.web_app_private_subnets[0].id,
 #     aws_subnet.web_app_private_subnets[1].id
 #   ]
-#   target_group_arns = [aws_lb_target_group.internal_alb_target_group.arn]
+#   target_group_arns = [aws_lb_target_group.external_alb_target_group.arn]
 
 #   mixed_instances_policy {
 #     launch_template {
 #       launch_template_specification {
-#         launch_template_id = aws_launch_template.ap_server_lt.id
+#         launch_template_id = aws_launch_template.web_server_lt.id
 #         version            = "$Latest"
 #       }
 #       override {
@@ -183,37 +156,64 @@ resource "aws_autoscaling_group" "web_server_asg" {
 #   }
 # }
 
-# -------------------------------------
-# Nat Gateway
-# -------------------------------------
-resource "aws_eip" "public_ngw_1a_eip" {
-  vpc = true
+# # APサーバー用のAuto Scaring Group
+# # resource "aws_autoscaling_group" "ap_server_asg" {
+# #   name                      = "${var.project}-${var.env}-asg-for-ap-server"
+# #   max_size                  = 4
+# #   min_size                  = 2
+# #   desired_capacity          = 2
+# #   health_check_grace_period = 300
+# #   health_check_type         = "ELB"
+# #   vpc_zone_identifier = [
+# #     aws_subnet.web_app_private_subnets[0].id,
+# #     aws_subnet.web_app_private_subnets[1].id
+# #   ]
+# #   target_group_arns = [aws_lb_target_group.internal_alb_target_group.arn]
 
-  tags = {
-    Name    = "${var.project}-${var.env}-public-ngw-1a-eip"
-    Project = var.project
-    Env     = var.env
-    Type    = "public"
-  }
-}
+# #   mixed_instances_policy {
+# #     launch_template {
+# #       launch_template_specification {
+# #         launch_template_id = aws_launch_template.ap_server_lt.id
+# #         version            = "$Latest"
+# #       }
+# #       override {
+# #         instance_type = var.default_instance_type
+# #       }
+# #     }
+# #   }
+# # }
 
-resource "aws_nat_gateway" "public_ngw_1a" {
-  allocation_id     = aws_eip.public_ngw_1a_eip.id
-  subnet_id         = aws_subnet.front_public_subnets[0].id
-  connectivity_type = "public"
+# # -------------------------------------
+# # Nat Gateway
+# # -------------------------------------
+# resource "aws_eip" "public_ngw_1a_eip" {
+#   vpc = true
 
-  tags = {
-    Name    = "${var.project}-${var.env}-public-ngw-1a"
-    Project = var.project
-    Env     = var.env
-    Type    = "public"
-  }
+#   tags = {
+#     Name    = "${var.project}-${var.env}-public-ngw-1a-eip"
+#     Project = var.project
+#     Env     = var.env
+#     Type    = "public"
+#   }
+# }
 
-  depends_on = [aws_internet_gateway.igw]
-}
+# resource "aws_nat_gateway" "public_ngw_1a" {
+#   allocation_id     = aws_eip.public_ngw_1a_eip.id
+#   subnet_id         = aws_subnet.front_public_subnets[0].id
+#   connectivity_type = "public"
 
-resource "aws_route" "web_app_private_rt_ngw_route" {
-  route_table_id         = aws_route_table.web_app_private_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_nat_gateway.public_ngw_1a.id
-}
+#   tags = {
+#     Name    = "${var.project}-${var.env}-public-ngw-1a"
+#     Project = var.project
+#     Env     = var.env
+#     Type    = "public"
+#   }
+
+#   depends_on = [aws_internet_gateway.igw]
+# }
+
+# resource "aws_route" "web_app_private_rt_ngw_route" {
+#   route_table_id         = aws_route_table.web_app_private_rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_nat_gateway.public_ngw_1a.id
+# }
--- network.tf
+++ network.tf
@@ -1,147 +1,147 @@
-# -------------------------------------
-# VPC
-# -------------------------------------
-resource "aws_vpc" "vpc" {
-  cidr_block                       = var.vpc_cidr
-  instance_tenancy                 = "default"
-  enable_dns_support               = true
-  enable_dns_hostnames             = true
-  assign_generated_ipv6_cidr_block = false
+# # -------------------------------------
+# # VPC
+# # -------------------------------------
+# resource "aws_vpc" "vpc" {
+#   cidr_block                       = var.vpc_cidr
+#   instance_tenancy                 = "default"
+#   enable_dns_support               = true
+#   enable_dns_hostnames             = true
+#   assign_generated_ipv6_cidr_block = false
 
-  tags = {
-    Name    = "${var.project}-${var.env}-vpc"
-    Project = var.project
-    Env     = var.env
-  }
-}
+#   tags = {
+#     Name    = "${var.project}-${var.env}-vpc"
+#     Project = var.project
+#     Env     = var.env
+#   }
+# }
 
-# -------------------------------------
-# Subnet
-# -------------------------------------
-resource "aws_subnet" "front_public_subnets" {
-  vpc_id                  = aws_vpc.vpc.id
-  count                   = length(var.vpc.front_public_subnets)
-  cidr_block              = var.front_public_subnet_cidrs[count.index]
-  availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = true
+# # -------------------------------------
+# # Subnet
+# # -------------------------------------
+# resource "aws_subnet" "front_public_subnets" {
+#   vpc_id                  = aws_vpc.vpc.id
+#   count                   = length(var.vpc.front_public_subnets)
+#   cidr_block              = var.front_public_subnet_cidrs[count.index]
+#   availability_zone       = var.availability_zones[count.index]
+#   map_public_ip_on_launch = true
 
-  tags = {
-    Name    = "${var.project}-${var.env}-${keys(var.vpc.front_public_subnets)[count.index]}"
-    Project = var.project
-    Env     = var.env
-    Type    = "public"
-  }
-}
+#   tags = {
+#     Name    = "${var.project}-${var.env}-${keys(var.vpc.front_public_subnets)[count.index]}"
+#     Project = var.project
+#     Env     = var.env
+#     Type    = "public"
+#   }
+# }
 
-resource "aws_subnet" "web_app_private_subnets" {
-  vpc_id                  = aws_vpc.vpc.id
-  count                   = length(var.vpc.web_app_private_subnets)
-  cidr_block              = var.web_app_private_subnet_cidrs[count.index]
-  availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = false
+# resource "aws_subnet" "web_app_private_subnets" {
+#   vpc_id                  = aws_vpc.vpc.id
+#   count                   = length(var.vpc.web_app_private_subnets)
+#   cidr_block              = var.web_app_private_subnet_cidrs[count.index]
+#   availability_zone       = var.availability_zones[count.index]
+#   map_public_ip_on_launch = false
 
-  tags = {
-    Name    = "${var.project}-${var.env}-${keys(var.vpc.web_app_private_subnets)[count.index]}"
-    Project = var.project
-    Env     = var.env
-    Type    = "private"
-  }
-}
+#   tags = {
+#     Name    = "${var.project}-${var.env}-${keys(var.vpc.web_app_private_subnets)[count.index]}"
+#     Project = var.project
+#     Env     = var.env
+#     Type    = "private"
+#   }
+# }
 
-resource "aws_subnet" "db_private_subnets" {
-  vpc_id                  = aws_vpc.vpc.id
-  count                   = length(var.vpc.db_private_subnets)
-  cidr_block              = var.db_private_subnet_cidrs[count.index]
-  availability_zone       = var.availability_zones[count.index]
-  map_public_ip_on_launch = false
+# resource "aws_subnet" "db_private_subnets" {
+#   vpc_id                  = aws_vpc.vpc.id
+#   count                   = length(var.vpc.db_private_subnets)
+#   cidr_block              = var.db_private_subnet_cidrs[count.index]
+#   availability_zone       = var.availability_zones[count.index]
+#   map_public_ip_on_launch = false
 
-  tags = {
-    Name    = "${var.project}-${var.env}-${keys(var.vpc.db_private_subnets)[count.index]}"
-    Project = var.project
-    Env     = var.env
-    Type    = "private"
-  }
-}
+#   tags = {
+#     Name    = "${var.project}-${var.env}-${keys(var.vpc.db_private_subnets)[count.index]}"
+#     Project = var.project
+#     Env     = var.env
+#     Type    = "private"
+#   }
+# }
 
-# -------------------------------------
-# Route Table
-# -------------------------------------
-# front public route table
-resource "aws_route_table" "front_public_rt" {
-  vpc_id = aws_vpc.vpc.id
+# # -------------------------------------
+# # Route Table
+# # -------------------------------------
+# # front public route table
+# resource "aws_route_table" "front_public_rt" {
+#   vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name    = "${var.project}-${var.env}-front-public-rt"
-    Project = var.project
-    Env     = var.env
-    Type    = "public"
-  }
-}
-resource "aws_route_table_association" "front_public_rt_1a" {
-  subnet_id      = aws_subnet.front_public_subnets[0].id
-  route_table_id = aws_route_table.front_public_rt.id
-}
-resource "aws_route_table_association" "front_public_rt_1c" {
-  subnet_id      = aws_subnet.front_public_subnets[1].id
-  route_table_id = aws_route_table.front_public_rt.id
-}
+#   tags = {
+#     Name    = "${var.project}-${var.env}-front-public-rt"
+#     Project = var.project
+#     Env     = var.env
+#     Type    = "public"
+#   }
+# }
+# resource "aws_route_table_association" "front_public_rt_1a" {
+#   subnet_id      = aws_subnet.front_public_subnets[0].id
+#   route_table_id = aws_route_table.front_public_rt.id
+# }
+# resource "aws_route_table_association" "front_public_rt_1c" {
+#   subnet_id      = aws_subnet.front_public_subnets[1].id
+#   route_table_id = aws_route_table.front_public_rt.id
+# }
 
-# web app private route table
-resource "aws_route_table" "web_app_private_rt" {
-  vpc_id = aws_vpc.vpc.id
+# # web app private route table
+# resource "aws_route_table" "web_app_private_rt" {
+#   vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name    = "${var.project}-${var.env}-web-app-private-rt"
-    Project = var.project
-    Env     = var.env
-    Type    = "private"
-  }
-}
-resource "aws_route_table_association" "web_app_private_rt_1a" {
-  subnet_id      = aws_subnet.web_app_private_subnets[0].id
-  route_table_id = aws_route_table.web_app_private_rt.id
-}
-resource "aws_route_table_association" "web_app_private_rt_1c" {
-  subnet_id      = aws_subnet.web_app_private_subnets[1].id
-  route_table_id = aws_route_table.web_app_private_rt.id
-}
+#   tags = {
+#     Name    = "${var.project}-${var.env}-web-app-private-rt"
+#     Project = var.project
+#     Env     = var.env
+#     Type    = "private"
+#   }
+# }
+# resource "aws_route_table_association" "web_app_private_rt_1a" {
+#   subnet_id      = aws_subnet.web_app_private_subnets[0].id
+#   route_table_id = aws_route_table.web_app_private_rt.id
+# }
+# resource "aws_route_table_association" "web_app_private_rt_1c" {
+#   subnet_id      = aws_subnet.web_app_private_subnets[1].id
+#   route_table_id = aws_route_table.web_app_private_rt.id
+# }
 
-# db private route table
-resource "aws_route_table" "db_private_rt" {
-  vpc_id = aws_vpc.vpc.id
+# # db private route table
+# resource "aws_route_table" "db_private_rt" {
+#   vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name    = "${var.project}-${var.env}-db-private-rt"
-    Project = var.project
-    Env     = var.env
-    Type    = "private"
-  }
-}
-resource "aws_route_table_association" "db_private_rt_1a" {
-  subnet_id      = aws_subnet.db_private_subnets[0].id
-  route_table_id = aws_route_table.db_private_rt.id
-}
-resource "aws_route_table_association" "db_private_rt_1c" {
-  subnet_id      = aws_subnet.db_private_subnets[1].id
-  route_table_id = aws_route_table.db_private_rt.id
-}
+#   tags = {
+#     Name    = "${var.project}-${var.env}-db-private-rt"
+#     Project = var.project
+#     Env     = var.env
+#     Type    = "private"
+#   }
+# }
+# resource "aws_route_table_association" "db_private_rt_1a" {
+#   subnet_id      = aws_subnet.db_private_subnets[0].id
+#   route_table_id = aws_route_table.db_private_rt.id
+# }
+# resource "aws_route_table_association" "db_private_rt_1c" {
+#   subnet_id      = aws_subnet.db_private_subnets[1].id
+#   route_table_id = aws_route_table.db_private_rt.id
+# }
 
-# -------------------------------------
-# Internet Gateway
-# -------------------------------------
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
+# # -------------------------------------
+# # Internet Gateway
+# # -------------------------------------
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name    = "${var.project}-${var.env}-igw"
-    Project = var.project
-    Env     = var.env
-  }
-}
+#   tags = {
+#     Name    = "${var.project}-${var.env}-igw"
+#     Project = var.project
+#     Env     = var.env
+#   }
+# }
 
-resource "aws_route" "front_public_rt_igw_route" {
-  route_table_id         = aws_route_table.front_public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
+# resource "aws_route" "front_public_rt_igw_route" {
+#   route_table_id         = aws_route_table.front_public_rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.igw.id
+# }
 
