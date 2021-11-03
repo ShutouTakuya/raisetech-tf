ebb99c6395784ad891642f288a352ff902612c08
takuya <8vc3260105@gmail.com>
2021-11-03 21:28:21 +0900

    Internal ALBの作成、セキュリティグループのアウトバウンド修正

--- s3.tf
+++ s3.tf
@@ -1,50 +1,50 @@
-# S3がグローバルでユニークになるようにランダム文字列を生成
-resource "random_string" "s3_unique_key" {
-  length  = 6
-  upper   = false
-  lower   = true
-  number  = true
-  special = false
-}
+# # S3がグローバルでユニークになるようにランダム文字列を生成
+# resource "random_string" "s3_unique_key" {
+#   length  = 6
+#   upper   = false
+#   lower   = true
+#   number  = true
+#   special = false
+# }
 
-# -------------------------------------
-# S3 Static Bucket
-# -------------------------------------
-resource "aws_s3_bucket" "s3_static_bucket" {
-  bucket = "${var.project}-${var.env}-s3-bucket-${random_string.s3_unique_key.result}"
+# # -------------------------------------
+# # S3 Static Bucket
+# # -------------------------------------
+# resource "aws_s3_bucket" "s3_static_bucket" {
+#   bucket = "${var.project}-${var.env}-s3-bucket-${random_string.s3_unique_key.result}"
 
-  versioning {
-    enabled = false
-  }
-}
+#   versioning {
+#     enabled = false
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "s3_static_bucket" {
-  bucket                  = aws_s3_bucket.s3_static_bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = false
+# resource "aws_s3_bucket_public_access_block" "s3_static_bucket" {
+#   bucket                  = aws_s3_bucket.s3_static_bucket.id
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = false
 
-  depends_on = [
-    aws_s3_bucket_policy.s3_static_bucket
-  ]
-}
+#   depends_on = [
+#     aws_s3_bucket_policy.s3_static_bucket
+#   ]
+# }
 
-resource "aws_s3_bucket_policy" "s3_static_bucket" {
-  bucket = aws_s3_bucket.s3_static_bucket.id
-  policy = data.aws_iam_policy_document.s3_static_bucket.json
-}
+# resource "aws_s3_bucket_policy" "s3_static_bucket" {
+#   bucket = aws_s3_bucket.s3_static_bucket.id
+#   policy = data.aws_iam_policy_document.s3_static_bucket.json
+# }
 
-data "aws_iam_policy_document" "s3_static_bucket" {
-  statement {
-    effect    = "Allow"
-    actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.s3_static_bucket.arn}/*"]
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-  }
-}
+# data "aws_iam_policy_document" "s3_static_bucket" {
+#   statement {
+#     effect    = "Allow"
+#     actions   = ["s3:GetObject"]
+#     resources = ["${aws_s3_bucket.s3_static_bucket.arn}/*"]
+#     principals {
+#       type        = "*"
+#       identifiers = ["*"]
+#     }
+#   }
+# }
 
 
